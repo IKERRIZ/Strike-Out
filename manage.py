462d714252dae3a003d619bfca80301e00802c36
IKERRIZ <okothfaith94@gmail.com>
2019-11-01 11:11:42 +0300

    Corrected the shell decorator

--- manage.py
+++ manage.py
@@ -8,7 +8,6 @@ app = create_app('development')
 manager= Manager(app)
 migrate = Migrate(app,db)
 
-
 manager.add_command('server', Server)
 manager.add_command('db',MigrateCommand)
 
@@ -22,6 +21,7 @@ def test():
 @manager.shell
 def make_shell_context():
     return dict(app = app,db = db,User = User )  
+    
 
 if __name__=='__main__':
     manager.run()      
